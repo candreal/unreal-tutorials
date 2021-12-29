48b3d72475182752b561097d71026353d5d8ce55
Celena <42121844+candreal@users.noreply.github.com>
2021-12-29 00:05:47 -0500

    Added welcome message, hidden word, and clearscreen method call

--- BullCowGame-starter-kit/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,12 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(TEXT("Welcome to Bull Cow Game"));
+    PrintLine(TEXT("Please input something, and then press enter"));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+    ClearScreen();
+    FString HiddenWord = TEXT("smite");
 }
